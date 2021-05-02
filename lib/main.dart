47d54a2f0c8f094f2b5acfeb14bdc9ea38cc1a4b
Ishaan Kakkar <ishaankakkar95@gmail.com>
2021-05-02 17:11:14 +0530

    [added] keypad inputs expression and basic evaluation logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:fabacus/style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:function_tree/function_tree.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Fabacus',
       theme: ThemeData.dark(),
@@ -22,6 +27,7 @@ class MyApp extends StatelessWidget {
 class CalculatorHomeScreen extends StatelessWidget {
   CalculatorHomeScreen({Key key, this.title}) : super(key: key);
   final String title;
+  final TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -38,9 +44,12 @@ class CalculatorHomeScreen extends StatelessWidget {
               children: [
                 Display(
                   displayHeight: displayHeight,
+                  controller: _controller,
                 ),
                 Expanded(
-                  child: KeyPad(),
+                  child: KeyPad(
+                    controller: _controller,
+                  ),
                 )
               ],
             ),
@@ -52,19 +61,47 @@ class CalculatorHomeScreen extends StatelessWidget {
 }
 
 class KeyButton extends StatelessWidget {
-  KeyButton({Key key, this.title}) : super(key: key);
+  KeyButton({Key key, this.title, this.controller}) : super(key: key);
   final String title;
+  final TextEditingController controller;
+
+  num doubleWithoutDecimalToInt(double val) {
+    return val % 1 == 0 ? val.toInt() : val;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(24.0),
-      child: Text(
-        title,
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          fontSize: 32.0,
-          fontWeight: FontWeight.w400,
-          color: keys,
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: () {
+          if (title == '=') {
+            String expression = controller.text;
+            expression = expression.replaceAll("×", "*");
+            expression = expression.replaceAll("−", "-");
+            expression = expression.replaceAll("÷", "/");
+            //print("Expression is $expression");
+            try {
+              double result = expression.interpret().toDouble();
+              controller.text = "${doubleWithoutDecimalToInt(result)}";
+            } catch (error) {
+              controller.text = "Invalid Expression";
+            }
+          } else {
+            controller?.text += title;
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Text(
+            title,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w400,
+              color: keys,
+            ),
+          ),
         ),
       ),
     );
@@ -72,34 +109,51 @@ class KeyButton extends StatelessWidget {
 }
 
 class OperatorButton extends StatelessWidget {
-  OperatorButton({Key key, this.title, this.iconData}) : super(key: key);
+  OperatorButton({Key key, this.title, this.iconData, this.controller})
+      : super(key: key);
   final String title;
   final IconData iconData;
+  final TextEditingController controller;
   @override
   Widget build(BuildContext context) {
     final color = HexColor.fromHex('#86A3DC');
-    return Padding(
-      padding: EdgeInsets.all(24.0),
-      child: iconData != null
-          ? Icon(
-              iconData,
-              size: 28.0,
-              color: color,
-            )
-          : Text(
-              title,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.w400,
-                color: color,
-              ),
-            ),
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: () {
+          if (title == 'clr') {
+            controller?.text = "";
+          } else {
+            controller?.text += title;
+          }
+        },
+        child: Padding(
+          padding: EdgeInsets.all(24.0),
+          child: iconData != null
+              ? Icon(
+                  iconData,
+                  size: 28.0,
+                  color: color,
+                )
+              : Text(
+                  title,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w400,
+                    color: color,
+                  ),
+                ),
+        ),
+      ),
     );
   }
 }
 
 class KeyPad extends StatelessWidget {
+  KeyPad({Key key, this.controller}) : super(key: key);
+  final TextEditingController controller;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -115,33 +169,33 @@ class KeyPad extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      KeyButton(title: '7'),
-                      KeyButton(title: '8'),
-                      KeyButton(title: '9'),
+                      KeyButton(title: '7', controller: controller),
+                      KeyButton(title: '8', controller: controller),
+                      KeyButton(title: '9', controller: controller),
                     ],
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      KeyButton(title: '4'),
-                      KeyButton(title: '5'),
-                      KeyButton(title: '6'),
+                      KeyButton(title: '4', controller: controller),
+                      KeyButton(title: '5', controller: controller),
+                      KeyButton(title: '6', controller: controller),
                     ],
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      KeyButton(title: '1'),
-                      KeyButton(title: '2'),
-                      KeyButton(title: '3'),
+                      KeyButton(title: '1', controller: controller),
+                      KeyButton(title: '2', controller: controller),
+                      KeyButton(title: '3', controller: controller),
                     ],
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      KeyButton(title: '0'),
-                      KeyButton(title: '.'),
-                      KeyButton(title: '='),
+                      KeyButton(title: '0', controller: controller),
+                      KeyButton(title: '.', controller: controller),
+                      KeyButton(title: '=', controller: controller),
                     ],
                   )
                 ],
@@ -164,18 +218,23 @@ class KeyPad extends StatelessWidget {
                   OperatorButton(
                     title: 'clr',
                     iconData: Icons.backspace_outlined,
+                    controller: controller,
                   ),
                   OperatorButton(
                     title: '÷',
+                    controller: controller,
                   ),
                   OperatorButton(
                     title: '×',
+                    controller: controller,
                   ),
                   OperatorButton(
                     title: '−',
+                    controller: controller,
                   ),
                   OperatorButton(
                     title: '+',
+                    controller: controller,
                   ),
                 ],
               ),
@@ -201,20 +260,33 @@ class KeyPad extends StatelessWidget {
   }
 }
 
-class Display extends StatelessWidget {
-  Display({Key key, this.displayHeight}) : super(key: key);
+class Display extends StatefulWidget {
+  Display({Key key, this.displayHeight, this.controller}) : super(key: key);
   final double displayHeight;
+  final TextEditingController controller;
+
+  @override
+  _DisplayState createState() => _DisplayState();
+}
+
+class _DisplayState extends State<Display> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: displayHeight,
+      height: widget.displayHeight,
       child: Container(
         color: inputBg,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Padding(
-              padding: const EdgeInsets.only(top: 16.0, left: 16.0, right: 8.0),
+              padding: EdgeInsets.only(top: 16.0, left: 16.0, right: 8.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -238,7 +310,30 @@ class Display extends StatelessWidget {
                   )
                 ],
               ),
-            )
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: TextField(
+                  showCursor: true,
+                  style: TextStyle(
+                    fontSize: 32.0,
+                  ),
+                  cursorColor: HexColor.fromHex('#86A3DC'),
+                  readOnly: true,
+                  autofocus: true,
+                  textAlign: TextAlign.right,
+                  controller: widget.controller,
+                  decoration: new InputDecoration(
+                    border: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    errorBorder: InputBorder.none,
+                    disabledBorder: InputBorder.none,
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
